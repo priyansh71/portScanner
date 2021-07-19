8482524ef70d891b357efa15650c93b0d726d529
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-20 00:16:39 +0530

    18 ports/sec

--- serve.py
+++ serve.py
@@ -6,9 +6,9 @@ start = time.time()
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-ip", help="I.P. address of victim.", required=True)
-parser.add_argument("-pR", help="Intakes two ports separated by spaces which are used as the bounds of scanning. (Both inclusive)", type=int, nargs="+")
-parser.add_argument("-op" , help="Returns a list of open ports and nothing if no ports are open. Use with either -pA or -pR.", action="store_true")
+parser.add_argument("-ip", required=True)
+parser.add_argument("-pR", type=int, nargs="+")
+parser.add_argument("-op" , action="store_true")
 
 args = parser.parse_args()
 
@@ -18,7 +18,7 @@ open = args.op
 
 def scan(host, port):
     s = socket.socket()
-    s.settimeout(0.07)
+    s.settimeout(0.049)
     try:
         s.connect((host, port))
         return True
@@ -28,14 +28,11 @@ def scan(host, port):
 def openRange(array):
     for port in range(array[0],array[1] +1):
         if scan(host, port):
-            print("Port", port, "is open.")
+            print(port, ": open.")
 
 if open:
-    # try:
-        openRange(portsRange)
-    # except:
-    #     print("Please provide valid arguments.")
+    openRange(portsRange)
+
 end = time.time()
 
-print("Execution completed in", round(end-start , 3), "seconds.")
-print("\n")
+print(round(end-start , 3), "seconds.")
